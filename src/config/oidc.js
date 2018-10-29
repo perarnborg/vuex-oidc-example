4e4e715288356a94f305b0052275e74278c97862
Per Arnborg <kontakt@perarnborg.se>
2018-10-29 23:15:42 +0100

    update vuex-oidc

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -2,7 +2,7 @@ export const oidcSettings = {
   authority: 'https://accounts.google.com',
   client_id: '459300396575-3ruj8l8jn69pcgst8rgkqnk6g43gbc78.apps.googleusercontent.com',
   redirect_uri: 'http://localhost:5002/oidc-callback',
-  response_type: 'id_token',
+  response_type: 'id_token token',
   scope: 'openid email',
   automaticSilentRenew: true,
   silent_redirect_uri: 'http://localhost:5002/silent-renew-oidc.html'
