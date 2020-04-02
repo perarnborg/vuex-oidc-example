b217b691d46930478afaae8290743db37c1a4054
Per Arnborg <kontakt@perarnborg.se>
2020-04-03 01:41:13 +0200

    Remove arg from process silent callback

--- src/silent-renew-oidc.js
+++ src/silent-renew-oidc.js
@@ -1,5 +1,4 @@
 import 'core-js/fn/promise'
 import { vuexOidcProcessSilentSignInCallback } from 'vuex-oidc'
-import { oidcSettings } from './config/oidc'
 
-vuexOidcProcessSilentSignInCallback(oidcSettings)
+vuexOidcProcessSilentSignInCallback()
